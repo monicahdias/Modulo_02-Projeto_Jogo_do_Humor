27e61db3ad94f2b9f08e7a9b6c39250ef1a1f7f1
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-03-15 11:16:46 -0300

    att

--- JS/script.js
+++ JS/script.js
@@ -7,25 +7,25 @@ function mudarImagem(){
     console.log(cont);
     if(cont == 1){
         document.getElementById("img1").src= "./images/img2.png";
-        document.getElementById("nome").innerText = "PREOCUPADA";
-        document.getElementById("situacao").innerText = "Quando será que eles vão arrumar um emprego?";
+        document.getElementById("tipo").innerText = "PREOCUPADA";
+        document.getElementById("situacao").innerText = "Quando eles vão arrumar um emprego?";
     }
     else if (cont == 2) {
         document.getElementById("img1").src ="./images/img3.png";
-        document.getElementById("nome").innerText = "ZEN";
+        document.getElementById("tipo").innerText = "ZEN";
         document.getElementById("situacao").innerText = "Não me perturba que hoje eu tô tranquila!";
     } else if (cont == 3) {
         document.getElementById("img1").src ="./images/img4.png";
-        document.getElementById("nome").innerText = "MANDONA";
+        document.getElementById("tipo").innerText = "MANDONA";
         document.getElementById("situacao").innerText = "Já FALEI pra você tomar banho, AGORA!";
     } else if (cont == 4) {
         document.getElementById("img1").src ="./images/img5.png";
-        document.getElementById("nome").innerText = "DRAMÁTICA";
+        document.getElementById("tipo").innerText = "DRAMÁTICA";
         document.getElementById("situacao").innerText = "Só vão me dar valor quando eu estiver MORTA!";
     }
     else {
         document.getElementById("img1").src ="./images/img1.png";
-        document.getElementById("nome").innerText = "PLENA";
+        document.getElementById("tipo").innerText = "PLENA";
         document.getElementById("situacao").innerText = "Vou bater perna até as crianças voltarem";
         cont = 0;
     }
